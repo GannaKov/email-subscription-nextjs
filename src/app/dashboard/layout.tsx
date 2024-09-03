d6689f8abe615717a40545d3e0cf850767cde6f2
GannaKov <zlatta2000@gmail.com>
2024-09-03 16:14:57 +0200

    first structure

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,9 +5,12 @@ import React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <Sidebar />
-      <main className="ml-60">{children}</main>
-    </>
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none overflow-y-auto md:w-60">
+        <Sidebar />
+      </div>
+
+      <main className="ml-0 px-3 py-4 md:px-2">{children}</main>
+    </div>
   );
 }
--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+'use client';
+import Link from 'next/link';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 
 export interface SidebarProps {}
 
 export default function Sidebar({}: SidebarProps) {
+  const pathname = usePathname();
   return (
     //<aside className="fixed left-0 top-0 z-40 h-screen w-60">
-    <aside>
-      <div className="flex h-full flex-col overflow-y-auto bg-gray-900 text-white">
-        Sidebar
-      </div>
-    </aside>
+
+    <div className="flex h-full flex-col bg-gray-900 px-3 py-4 text-white md:px-2">
+      <Link
+        href="dashboard/users"
+        className={clsx(
+          'h-[48px] rounded-md p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:px-3',
+          {
+            'bg-sky-100 text-blue-600': pathname === '/users',
+          }
+        )}
+      >
+        Users
+      </Link>
+    </div>
   );
 }
